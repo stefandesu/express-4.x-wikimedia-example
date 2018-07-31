660a31d24474c96b22d828dffa44e3bc15b30773
Stefan Peters <Stefan.Peters@gbv.de>
2018-07-31 08:21:44 +0200

    Replace all Facebook occurrences with GitHub

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 var port = process.env.PORT || 3000;
 
-// Configure the Facebook strategy for use by Passport.
+// Configure the GitHub strategy for use by Passport.
 //
 // OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
+// credential (`accessToken`) for accessing the GitHub API on the user's
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
@@ -18,8 +18,8 @@ passport.use(new Strategy({
     callbackURL: `http://localhost:${port}/login/github/return`
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
+    // In this example, the user's GitHub profile is supplied as the user
+    // record.  In a production-quality application, the GitHub profile should
     // be associated with a user record in the application's database, which
     // allows for account linking and authentication with other identity
     // providers.
@@ -34,7 +34,7 @@ passport.use(new Strategy({
 // production-quality application, this would typically be as simple as
 // supplying the user ID when serializing, and querying the user record by ID
 // from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
+// example does not have a database, the complete GitHub profile is serialized
 // and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
